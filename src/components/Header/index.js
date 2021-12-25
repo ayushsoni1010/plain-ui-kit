f7a63cca57c56b7a0de0bac3e6e55a0f577cc93c
Yash Sehgal <yashsehgal.work@gmail.com>
2021-12-25 15:05:07 +0000

    style: CSS methods added for header component structure

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,20 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './header.component.style.css';
 
 export default function Header({headerConfiguration}) {
     let logoData = {
         url: headerConfiguration.logo.imgURL,
         title: headerConfiguration.logo.title,
-        route: headerConfiguration.logo.route
+        route: headerConfiguration.logo.route,
+        className: (headerConfiguration.logo.className ? headerConfiguration.logo.className : '')
     };
     let styleConfiguration = {
         backgroundColor: headerConfiguration.themeConfig.backgroundColor,
         shadow: (headerConfiguration.themeConfig.shadow ? 'header-shadow' : 'NO_SHADOW'),
         textColor: headerConfiguration.themeConfig.textColor
     };
+    const [headerOptionsRef] = useState(headerConfiguration.options);
     console.log(logoData);
     console.log(styleConfiguration);
     return (
@@ -22,7 +26,52 @@ export default function Header({headerConfiguration}) {
                     boxShadow: `var(--${styleConfiguration.shadow})`
                 }}
             >
-                
+                <div className="logo-title-content-wrapper">
+                    <React.Fragment>
+                        <img src={logoData.url} alt={logoData.title}
+                            className={`header-logo`}
+                        />
+                    </React.Fragment>
+                </div>
+                <div className="header-options-cta-container">
+                    {headerOptionsRef.map((option, index) => {
+                        if (option.type === 'link') {
+                            if (option.route === undefined) {
+                                return <p className="header-option__text">{option.name}</p>
+                            } else {
+                                return (
+                                    <Link to={option.route} className="header-option__link">
+                                        <a target="_blank" rel="noreferrer" className="header-option__link">{option.name}</a>                                        
+                                    </Link>
+                                )
+                            }
+                        } else if (option.type === 'button') {
+                            if (option.route === undefined) {
+                                return (
+                                    <button
+                                        className={
+                                            `header-option__button ${option.buttonType}`
+                                        }
+                                    >
+                                        {option.name}
+                                    </button>
+                                )
+                            } else {
+                                return (
+                                    <Link to={option.route}>
+                                        <button
+                                            className={
+                                                `header-option__button ${option.buttonType}`
+                                            }
+                                        >
+                                            {option.name}
+                                        </button>
+                                    </Link>
+                                )
+                            }
+                        }
+                    })}
+                </div>
             </div>
         </div>
     )
