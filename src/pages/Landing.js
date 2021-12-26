8cca58fbb37973daa5d07be5a2e62b2b51e61759
Yash Sehgal <yashsehgal.work@gmail.com>
2021-12-26 15:35:29 +0000

    fix: routes removed from demo header-component from docs

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -17,12 +17,12 @@ export default function Landing() {
             logoShape: 'circle'
         },
         options: [
-            { type: 'link', name: 'Documentation', route: '/docs' },
-            { type: 'link', name: 'GitHub', route: '/github' },
-            { type: 'link', name: 'Support Us', route: '/support-donate' },
-            { type: 'link', name: 'Discord', route: '/discord' },
-            { type: 'button', buttonVariant: 'button-outline', name: 'Login', route: '/login' },
-            { type: 'button', buttonVariant: 'button-solid', name: 'Create an Account' }
+            { type: 'link', name: 'Documentation' },
+            { type: 'link', name: 'GitHub' },
+            { type: 'link', name: 'Support Us' },
+            { type: 'link', name: 'Discord' },
+            { type: 'button', buttonVariant: 'button-outline', name: 'Login' },
+            { type: 'button', buttonVariant: 'button-solid' }
         ],
         themeConfig: {
             backgroundColor: 'white',
